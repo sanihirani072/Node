93c74bacdee6ebad1cb0268990d44501aa18e169
SUNNY HIRANI <sanihirani3@gmail.com>
2026-04-04 16:10:37 +0530

    task manager complete - rename,eidt and view

--- Note-Node/index.js
+++ Note-Node/index.js
@@ -1,24 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const fs = require("fs");
 
+// Data read by express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Data Read by express
-app.use(express.json()); //Body Json Data Read
-app.use(express.urlencoded({extended: true})); //Body Form Data Read
+// ejs setup
+app.set("view engine", "ejs");
 
+// Home Page
+app.get("/", (req, res) => {
+    fs.readdir("./tasks", (e, files) => {
+        if (e) throw e;
 
-//ejs setup
-app.set("view engine","ejs");
-//ejs -->view 
-
-
-app.get('/',(req,res)=>{
-    res.render('index');
+        res.render("index.ejs", { files });
+    });
 });
 
-//Task File Manager --> Create a file for every task (Tasks Folder) 
-//red, red.parmas , req.body
+// Create File
+app.post("/create", (req, res) => {
+    let data = `Title: ${req.body.title}\nDetails: ${req.body.task}`;
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+    fs.writeFile(
+        `./tasks/${req.body.title.split(" ").join("-")}.txt`,
+        data,
+        (e) => {
+            if (e) throw e;
+
+            res.redirect("/");
+        }
+    );
+});
+
+// Read File
+app.get("/files/:filename", (req, res) => {
+    fs.readFile(`./tasks/${req.params.filename}`, "utf-8", (e, data) => {
+        if (e) {
+            console.log(e);
+            return res.send("File not found");
+        }
+
+        res.render("read", {
+            title: req.params.filename,
+            task: data
+        });
+    });
+});
+
+// Open Edit Page
+app.get("/edit/:filename", (req, res) => {
+    res.render("edit", { filename: req.params.filename });
+});
+
+// Rename File
+app.post("/edit", (req, res) => {
+    const oldPath = `./tasks/${req.body.oldName}`;
+    const newPath = `./tasks/${req.body.newName}.txt`;
+
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+            console.log(err);
+            return res.send("File rename failed");
+        }
+
+        res.redirect("/");
+    });
+});
+
+
+// update file namne
+ 
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
